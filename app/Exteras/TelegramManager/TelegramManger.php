63ea2d43ec54c0b2ada3c59c0a338244deb4b795
root <root@homestead>
2018-01-30 09:43:55 +0000

    Telegram Manager

---
+++ app/Exteras/TelegramManager/TelegramManger.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: husseinmirzaki
+ * Date: 30/01/2018
+ * Time: 05:26 PM
+ */
+
+namespace App\Exteras\TelegramManager;
+
+
+class TelegramManger
+{
+
+}
