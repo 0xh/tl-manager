39bb9d07af6a74f8052d0109903c83a40615331d
Hussein Mirzaki <husseinmirzaki@gmail.com>
2018-01-31 18:23:22 +0330

    tests

--- app/Extras/TelegramManager/Madeline/TelegramManager.php
+++ app/Extras/TelegramManager/Madeline/TelegramManager.php
@@ -9,7 +9,6 @@ use App\Extras\TelegramManager\Madeline\Traits\SessionTrait;
 use App\Extras\TelegramManager\Madeline\Traits\TypesTrait;
 use App\Extras\TelegramManager\Madeline\Traits\UpdatesTrait;
 use danog\MadelineProto\API;
-use danog\MadelineProto\Exception;
 
 class TelegramManager
 {
@@ -27,44 +26,54 @@ class TelegramManager
      */
     protected $api;
 
-    public function __construct($options = [])
+    /**
+     * TelegramManager constructor.
+     * @param $options
+     * @throws \Exception
+     */
+    public function __construct($options)
     {
         if (is_array($options) && isset($options['session'])) {
             $this->session = $options['session'];
             unset($options['session']);
-        } else if (is_string($options) && !$options) {
+        } else if (is_string($options) && $options) {
             $this->session = $options;
         } else {
             throw new \Exception('What about your session');
         }
-
-        if (!empty($options)) {
-            if (is_string($options)) {
-                try {
-                    $this->loadSession($this->session);
-                    return;
-                } catch (\Exception $exception) {
-                    echo "Problem Occurred : " . $exception->getMessage();
-                }
+        if (file_exists($this->getSessionWithDIR())) {
+            try {
+                $this->loadSession($this->getSessionWithDIR());
+                return;
+            } catch (\Exception $exception) {
+                echo "Problem Occurred : " . $exception->getMessage() . PHP_EOL;
             }
-            $this->createNewSession($options);
         }
+        echo 'Creating a new MadelineProto instance ...' . PHP_EOL;
+        $this->createNewSession($options);
     }
 
-    public function loadSession($options)
+    public function loadSession(string $session)
     {
-        $this->api = new API($this->session);
+        $this->api = new API($session);
     }
 
     public function createNewSession($options)
     {
+        # my default options
         $default = [
             'app_info' => [
                 'api_id' => env('TL_API_ID', 6),
                 'api_hash' => env('TL_API_HASH', 'b06d4abfb49dc3eeb1aeb98ae0f581e'),
             ]
         ];
+        if (is_string($options)) {
+            $options = [];
+        }
         if (!isset($options['app_info'])) {
+            $options = [
+                'app_info' => [],
+            ];
             $options['app_info'] = $default['app_info'];
         } else {
             isset($options['app_info']['api_id']) ?: $options['app_info']['api_id'] = $default['app_info']['api_id'];
@@ -72,8 +81,10 @@ class TelegramManager
         }
         try {
             $this->api = new API($options);
+            echo 'We created a new instance of MadelineProto (lol) ' . PHP_EOL;
+            echo "Call it by using this session name -> '" . $this->session . "'" . PHP_EOL;
             $this->serializeSession();
-        } catch (Exception $exception) {
+        } catch (\Exception $exception) {
             echo "Problem occurred : " . $exception->getMessage();
             $this->fails[] = $exception;
             if ($this->failsCount() < $this->max_creation_try) {
